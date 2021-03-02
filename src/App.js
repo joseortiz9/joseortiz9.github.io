47d93cb9806fb313a091622c0917b22648bc1cba
jose <joseco04@live.com>
2021-03-02 18:16:09 -0500

    initial content without styles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{display: "flex", justifyContent: "center", alignItems: "center"}}>
+      <div style={{display: "flex", flexDirection: "column", alignItems: "center"}}>
+        <h1>Jose David Ortiz Correa</h1>
+        <h3>Full-stack developer</h3>
+        <h4>Colombian ITMO student</h4>
+        <div>
+          <a href="https://github.com/joseortiz9">Repositories</a>
+        </div>
+      </div>
     </div>
   );
 }
